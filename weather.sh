0070ada8414aa46d42aa51542ffd88cd40448ef6
Vraj Shah <vas4@njit.edu>
2022-04-18 22:02:28 -0400

    Basic Stuff

---
+++ weather.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Remove all files if the user quites the program
+trap 'rm parsippany.* newark.* atlantic_city.* princeton.* salem.*; exit' SIGINT
+
+# while loop to run indefinitely
+while :
+do
+    # Download the html files from the webstie
+    echo "DOWNLOADING HTML FILES"
+    wget -qO parsippany.html "https://forecast.weather.gov/MapClick.php?CityName=Parsippany&state=NJ"
+    wget -qO newark.html "https://forecast.weather.gov/MapClick.php?CityName=Newark&state=NJ"
+    wget -qO atlantic_city.html "https://forecast.weather.gov/MapClick.php?CityName=Atlantic+City&state=NJ"
+    wget -qO princeton.html "https://forecast.weather.gov/MapClick.php?CityName=Princeton&state=NJ"
+    wget -qO salem.html "https://forecast.weather.gov/MapClick.php?CityName=Salem&state=NJ"
+
+    #Convert the html files to xml using tagsoup
+    echo "CHECKING FOR TAGSOUP.jar"
+    FILENAME="tagsoup-1.2.1.jar"
+    if [ ! -f "$FILENAME" ]; then
+        wget -qO tagsoup-1.2.1.jar "https://repo1.maven.org/maven2/org/ccil/cowan/tagsoup/tagsoup/1.2.1/tagsoup-1.2.1.jar"
+    fi
+
+    echo "CONVERTING HTML TO XML"
+    java -jar tagsoup-1.2.1.jar --files parsippany.html newark.html atlantic_city.html princeton.html salem.html 
+    
+    #Use XML minidom with python script to parse data
+
+    #Remove the files
+    sleep 5
+    echo "REMOVING FILES"
+    rm parsippany.* newark.* atlantic_city.* princeton.* salem.*
+    
+    #Sleep for 6 hours to run again
+    echo "SLEEPING FOR 6 HOURS"
+    sleep 6h
+done
+
