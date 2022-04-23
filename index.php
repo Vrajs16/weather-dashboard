991ce6dffb473309d6d3951edb9cb3b4843b8a43
Vraj Shah <vas4@njit.edu>
2022-04-22 23:05:48 -0400

    Created Weather Ui

--- index.php
+++ index.php
@@ -43,6 +43,24 @@ if (!empty($city)) {
 
 $cursor = getCityWeather($city);
 $row = $cursor->fetch_assoc();
+
+function iconToChoose($desc)
+{
+  $arr = explode(" ", $desc);
+  $arr = array_map('strtolower', $arr);
+  if (in_array("sunny", $arr)) {
+    return '<i class="fa-solid fa-sun fa-4x"></i>';
+  } else if (in_array("cloudy", $arr)) {
+    return '<i class="fa-solid fa-cloud fa-4x"></i>';
+  } else if (in_array("showers", $arr)) {
+    return '<i class="fa-solid fa-cloud-rain fa-4x"></i>';
+  } else if (in_array("rain", $arr)) {
+    return '<i class="fa-solid fa-cloud-rain fa-4x"></i>';
+  } else {
+    return '<i class="fa-solid fa-temperature-half fa-4x"></i>';
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -53,6 +71,7 @@ $row = $cursor->fetch_assoc();
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="index.css" />
+  <script src="https://kit.fontawesome.com/105c450497.js" crossorigin="anonymous"></script>
   <title>Weather Dashboard</title>
 </head>
 
@@ -131,48 +150,71 @@ $row = $cursor->fetch_assoc();
       </div>
     </div>
   </div>
-
+  <div class="top-navigation">
+    <?php
+    if (!empty($row['last_update'])) {
+      $time = date('m-d-Y \a\t h:i A', strtotime($row['last_update']));
+    } else {
+      $time = "ERROR";
+    }
+    ?>
+    <h2 id="scraper-title" class="dash-title">Web Scraper Last Ran: <?php echo $time ?></h2>
+  </div>
   <div id="weather-container" class="weather-dashboard">
     <?php
     if (is_null($row)) {
       echo "SOME ERROR HAS OCCURED. PLEASE TRY AGAIN LATER OR NO DATA IN DB?";
     } else { ?>
       <div class='weather-item'>
-        <div>
-          <?php echo $row['day'] ?>
+        <div style="padding: 10px">
+          <h1 style="text-align:center"> <?php echo $row['day'] ?></h1>
+        </div>
+        <div style="padding-bottom:10px;display:flex; align-items:stretch; justify-content: space-between; flex-wrap: wrap; gap: 10px">
+          <div>
+            <h2><?php echo $row['temperature_text'] ?></h2>
+          </div>
+          <div>
+            <h2><?php echo $row['short_description'] ?></h2>
+          </div>
         </div>
         <div>
-          <?php echo $row['temperature_text'] ?>
+          <h3>Description:</h3>
+          <h3><?php echo substr($row['long_description'], strpos($row['long_description'], ":") + 1) ?></h3>
         </div>
-        <div>Icon</div>
-        <div>
-          <?php echo $row['short_description'] ?>
+        <div style="text-align: center; padding-top: 30px;">
+          <?php echo iconToChoose($row['short_description']) ?>
         </div>
-        <div>
-          <?php echo $row['long_description'] ?>
-        </div>
-        <div>
-          <?php echo $row['last_forecast_update'] ?>
+        <div style="text-align: center;padding-top:40px;">
+          <?php
+          $forecastTime = date('m-d-Y \a\t h:i A', strtotime($row['last_forecast_update'])); ?>
+          <h4>Last Forecast Update<br><?php echo $forecastTime ?></h4>
         </div>
       </div>
       <?php
       while ($row = $cursor->fetch_assoc()) { ?>
         <div class='weather-item'>
-          <div>
-            <?php echo $row['day'] ?>
+          <div style="padding: 10px">
+            <h1 style="text-align:center"> <?php echo $row['day'] ?></h1>
+          </div>
+          <div style="padding-bottom:10px;display:flex; align-items:stretch; justify-content: space-between; flex-wrap: wrap; gap: 10px">
+            <div>
+              <h2><?php echo $row['temperature_text'] ?></h2>
+            </div>
+            <div>
+              <h2><?php echo $row['short_description'] ?></h2>
+            </div>
           </div>
           <div>
-            <?php echo $row['temperature_text'] ?>
+            <h3>Description:</h3>
+            <h3><?php echo substr($row['long_description'], strpos($row['long_description'], ":") + 1) ?></h3>
           </div>
-          <div>Icon</div>
-          <div>
-            <?php echo $row['short_description'] ?>
+          <div style="text-align: center; padding-top: 30px;">
+            <?php echo iconToChoose($row['short_description']) ?>
           </div>
-          <div>
-            <?php echo $row['long_description'] ?>
-          </div>
-          <div>
-            <?php echo $row['last_forecast_update'] ?>
+          <div style="text-align: center;padding-top:40px;">
+            <?php
+            $forecastTime = date('m-d-Y \a\t h:i A', strtotime($row['last_forecast_update'])); ?>
+            <h4>Last Forecast Update<br><?php echo $forecastTime ?></h4>
           </div>
         </div>
     <?php
@@ -192,6 +234,7 @@ $row = $cursor->fetch_assoc();
   let cityTitle = document.getElementById('city-title');
   let backgroundTitle = document.getElementById('background-title');
   let weatherContainer = document.getElementById('weather-container');
+  let scraperTitle = document.getElementById('scraper-title');
 
   colorPicker.addEventListener("input", function() {
     let color = colorPicker.value;
@@ -207,12 +250,14 @@ $row = $cursor->fetch_assoc();
       weatherTitle.style.color = "black";
       cityTitle.style.color = "black";
       backgroundTitle.style.color = "black";
+      scraperTitle.style.color = "black";
       weatherContainer.style.backgroundColor = "black";
     } else {
       //set the text to white
       weatherTitle.style.color = "white";
       cityTitle.style.color = "white";
       backgroundTitle.style.color = "white";
+      scraperTitle.style.color = "white";
       weatherContainer.style.backgroundColor = "white";
     }
   });
@@ -230,12 +275,14 @@ $row = $cursor->fetch_assoc();
       cityTitle.style.color = "black";
       backgroundTitle.style.color = "black";
       weatherContainer.style.backgroundColor = "black";
+      scraperTitle.style.color = "black";
     } else {
       //set the text to white
       weatherTitle.style.color = "white";
       cityTitle.style.color = "white";
       backgroundTitle.style.color = "white";
       weatherContainer.style.backgroundColor = "white";
+      scraperTitle.style.color = "white";
     }
   });
 </script>
