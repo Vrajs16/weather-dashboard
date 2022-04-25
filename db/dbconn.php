bdae957a1b46a8643793665189727ea94deca85a
Vrajs16 <Vrajs16108@gmail.com>
2022-04-24 20:13:04 -0400

    UPDATED ENV VARIABLE

--- db/dbconn.php
+++ db/dbconn.php
@@ -2,7 +2,7 @@
 function mysqlConnect()
 {
     $DBHOST = getenv("DB_HOST");
-    $PASSWORD = getenv("MYSQL_PASSWORD");
+    $PASSWORD = getenv("DB_PASSWORD");
     $cnx = new mysqli($DBHOST, 'sql5487808', $PASSWORD, 'sql5487808');
     if ($cnx->connect_error)
         die('Connection failed: ' . $cnx->connect_error);
